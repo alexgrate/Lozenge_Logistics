5c7bb8957f1249f510c53acada1af7ab2a4b9f86
Aigbodion Dominion <116392457+alexgrate@users.noreply.github.com>
2023-07-27 10:30:11 +0100

    test run commit 07

--- Users/forms.py
+++ Users/forms.py
@@ -12,6 +12,14 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Password Confirmation'
 
 
 SHIPPING_OPTION = (
